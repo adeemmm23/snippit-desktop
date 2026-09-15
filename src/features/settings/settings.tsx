25f70f07e888c18439bd35d2ca0fad6d2c37ee53
Adem <112782893+adeemmm23@users.noreply.github.com>
2026-09-15 19:46:53 +0100

    fix: adjust setting height

--- src/features/settings/settings.tsx
+++ src/features/settings/settings.tsx
@@ -12,7 +12,7 @@ import { Separator } from "@/components/ui/separator";
 
 export default function Settings() {
   return (
-    <DialogContent className="bg-popover flex h-full max-h-11/12 w-full max-w-5xl! flex-col data-nested-dialog-open:scale-95">
+    <DialogContent className="bg-popover flex h-[calc(100%-6rem)] w-full max-w-5xl! flex-col data-nested-dialog-open:scale-95">
       <DialogHeader className="shrink-0 select-none">
         <DialogTitle className="text-2xl">Settings</DialogTitle>
         <DialogDescription>
