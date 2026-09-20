ac90b88cc3432be1a0d35d8dfa80b4877945a413
Adem <112782893+adeemmm23@users.noreply.github.com>
2026-09-21 00:06:34 +0100

    fix: adjust settings text

--- src/features/settings/settings.tsx
+++ src/features/settings/settings.tsx
@@ -15,10 +15,7 @@ export default function Settings() {
     <DialogContent className="bg-popover flex h-[calc(100%-6rem)] w-full max-w-5xl! flex-col data-nested-dialog-open:scale-95">
       <DialogHeader className="shrink-0 select-none">
         <DialogTitle className="text-2xl">Settings</DialogTitle>
-        <DialogDescription>
-          Settings are saved automatically. Changes will be reflected
-          immediately.
-        </DialogDescription>
+        <DialogDescription>Settings are saved automatically.</DialogDescription>
       </DialogHeader>
       <div className="flex min-h-0 flex-1 grow gap-2 py-2">
         <SectionsProvider>
